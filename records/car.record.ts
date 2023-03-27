b7f9b787d3803e9b78af5851280b5eb2c81ddc61
Marcin <116343437+Marcinolog@users.noreply.github.com>
2023-03-27 21:42:53 +0200

    Added validation to car.record.ts

--- records/car.record.ts
+++ records/car.record.ts
@@ -8,10 +8,39 @@ export class CarRecord {
     brand: string
     model: string
     engine: string
-    productionYear: Date
+    productionYear: number
 
     constructor(obj: CarRecord) {
         const {id, plateNumber, brand, model, engine, productionYear} = obj;
+        const productionYearToString = productionYear.toString()
+
+        if (!plateNumber) {
+            throw new Error("Plate number was not entered on the form")
+        } else if (plateNumber.length <5 || plateNumber.length > 7){
+            throw new Error("Plate number should have 5 to 7 characters")
+        }
+        if (!brand) {
+            throw new Error("Car brand was not entered on the form")
+        } else if (brand.length <3 || brand.length > 15) {
+            throw new Error("Brand name should have 3 to 15 characters")
+        }
+        if (!model) {
+            throw new Error('Car model was not entered on the form')
+        } else if (model.length <2 || model.length > 25) {
+            throw new Error("Model name should have 2 to 25 characters ")
+        }
+        if (!engine) {
+            throw new Error("Engine was not entered on the form")
+        }
+        if (!productionYear) {
+            throw new Error("Production year was not entered on the form")
+        } else if (isNaN(productionYear)) {
+            throw new Error("Production year have to be a number")
+        } else if (productionYearToString.length !== 4) {
+            throw new Error("The year of production must consist of four numbers")
+        }
+
+
         this.id = id ?? uuid();
         this.plateNumber = plateNumber;
         this.brand = brand;
--- routers/delegations.ts
+++ routers/delegations.ts
@@ -21,30 +21,3 @@ delegationsRouter
 
         await delegation.insert();
     });
-
-
-// import express from "express";
-// import {DelegationRecord} from "../records/delegation.record";
-//
-//
-// export const delegationsRouter = express.Router();
-//
-// delegationsRouter
-//     .get('/all-delegations', async (req, res) => {
-//         res.json(await DelegationRecord.listAll());
-//     })
-//
-//     .post('/add-delegation', async (req, res) => {
-//         const {serviceLocation, servicers, carPlateNumber, dateOfStart, dateOfEnd} = req.body
-//
-//         const delegation = new DelegationRecord({
-//             ...req.body,
-//             serviceLocation,
-//             servicers,
-//             carPlateNumber,
-//             dateOfStart,
-//             dateOfEnd,
-//         });
-//
-//         await delegation.insert();
-//     });
