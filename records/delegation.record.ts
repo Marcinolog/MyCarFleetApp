adc56d33fa46f052ce09cbccce803f146c0e5c64
Marcin <116343437+Marcinolog@users.noreply.github.com>
2023-03-27 22:18:27 +0200

    Added validation to delegations.record.ts

--- records/delegation.record.ts
+++ records/delegation.record.ts
@@ -15,6 +15,29 @@ type DelegationRecordResults = [DelegationRecord[], FieldPacket[]]
 
     constructor(obj: DelegationRecord) {
         const {id, serviceLocation, servicers, carPlateNumber, dateOfStart, dateOfEnd} = obj;
+
+        if (!serviceLocation) {
+            throw new Error("Service location was not entered on form")
+        } else if (serviceLocation.length <3 || serviceLocation.length > 40) {
+            throw new Error("Service location name should have 3 to 40 characters")
+        }
+        if (!servicers) {
+            throw new Error("Servicers where not entered on form")
+        } else if (servicers.length <3 || servicers.length > 20) {
+            throw new Error("Servicers surname should have 3 to 20 characters")
+        }
+        if (!carPlateNumber) {
+            throw new Error("Car plate number was not entered on form")
+        } else if (carPlateNumber.length <5 || carPlateNumber.length >7) {
+            throw new Error("Car plate number should have 5 to 7 characters")
+        }
+        if (!dateOfStart) {
+            throw new Error("Date of service start was not entered on form")
+        }
+        if (!dateOfEnd) {
+            throw new Error("Date of service end was not entered on form")
+        }
+
         this.id = id ?? uuid();
         this.serviceLocation = serviceLocation;
         this.servicers = servicers;
